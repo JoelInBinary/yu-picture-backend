3064b97637a8c11afcf0c853382d9bf8b55c63a9
joel <1489526569@qq.com>
2025-04-05 16:28:20 +0800

    初始化后端

---
+++ src/main/java/com/joel/yupicturebackend/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.joel.yupicturebackend.controller;
+
+
+import com.joel.yupicturebackend.common.BaseResponse;
+import com.joel.yupicturebackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+
+}
---
+++ src/test/java/com/joel/yupicturebackend/YuPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.yupicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
